5092215351f282cdf3abd2074091c74be078597a
Adam <adam@radical-departures.com>
2020-12-22 22:27:41 +0000

    added noise filter

--- Assets/Scripts/NoiseFilter.cs
+++ Assets/Scripts/NoiseFilter.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class NoiseFilter
 {
+
     Noise noise = new Noise();
+    NoiseSettings settings;
+
+    public NoiseFilter(NoiseSettings settings)
+    {
+       this.settings = settings; 
+    }
 
     public float Evaluate(Vector3 point)
     {
-        return (noise.Evaluate(point) + 1) * .5f;
+        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * .5f;
+        return noiseValue * settings.strength;
     }
 
 }
